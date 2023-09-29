33354d1949de07c884bf91600386e90b0417261c
Evandro da Costa <evandrodacosta@gmail.com>
2023-09-28 21:24:20 -0300

    initial and final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect } from 'react';
+import Select from 'react-select';
+import Card from './SummaryCard';
+
+
 function App() {
+
+  const locationList = [
+    { value: "AB", label: "Alberta" },
+    { value: "BC", label: "British Columbia" },
+    { value: "MB", label: "Manitoba" },
+    { value: "NB", label: "New Brunswick" },
+    { value: "NL", label: "Newfoundland and Labrador" },
+    { value: "NT", label: "Northwest Territories" },
+    { value: "NS", label: "Nova Scotia" },
+    { value: "NU", label: "Nunavut" },
+    { value: "ON", label: "Ontario" },
+    { value: "PE", label: "Prince Edward Island" },
+    { value: "QC", label: "Quebec" },
+    { value: "SK", label: "Saskatchewan" },
+    { value: "YT", label: "Yukon" },
+  ];
+
+  const [activeLocation, setActiveLocation] = useState("AB");
+  const [lastUpdated, setLastUpdated] = useState("");
+  const [summaryData, setSummaryData] = useState({})
+
+  const baseUrl = "https://api.opencovid.ca";
+  const getVersion = async () => {
+    const res = await fetch(`${baseUrl}/version`);
+    const data = await res.json();
+    setLastUpdated(data.timeseries);
+  }
+  const getSummaryData = async (location) => {
+    if (activeLocation === "canada") {
+      return;
+    }
+    let res = await fetch(`${baseUrl}/summary?loc=${activeLocation}`);
+    let resData = await res.json();
+    let summaryData = resData.data[0];
+    let formattedData = {};
+
+    Object.keys(summaryData).map(
+      (key) => (formattedData[key] = summaryData[key].toLocaleString())
+    );
+    console.log(formattedData);
+    setSummaryData(formattedData);
+  }
+
+  
+
+  useEffect(() => {
+    getSummaryData();
+    getVersion();
+  }, [activeLocation]);
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Covid 19 Dashboard</h1>
+      <div className='dashboard-container'>
+        <div className='dashboard-menu'>
+          <Select 
+            options={locationList}
+            onChange={(selectedOption) =>
+              setActiveLocation(selectedOption.value)
+            }
+            defaultValue={locationList.filter(
+              (options) => options.value === activeLocation
+            )}
+            className='dashboard-select'
+          />
+          <p className='update-date'>Last update: {lastUpdated}</p>
+        </div>
+        <div className='dashboard-summary'>
+          <Card title="Total Cases" value={summaryData.cases} />
+          <Card
+              title="Total Tests"
+              value={summaryData.tests_completed}
+            />
+            <Card title="Total Deaths" value={summaryData.deaths} />
+            <Card
+              title="Total Vaccinated"
+              value={summaryData.vaccine_administration_total_doses}
+            />
+        </div>
+      </div>
     </div>
   );
 }
